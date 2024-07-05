2938aac06faf0f5a2f8e6fe40c4d752685ca5b43
swath <swathykrishnakp159@gmail.com>
2024-07-05 11:08:09 +0530

    Add project files.

---
+++ MVCEntityAPI/API/APIController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+
+namespace MVCEntityAPI.API
+{
+    public class APIController : ApiController
+    {
+        // GET: api/API
+        Employee1Entities dbobj = new Employee1Entities();
+        [HttpGet]
+        [Route("api/API/getwebapitabs")]
+        public IHttpActionResult Get()
+        {
+            return Ok(dbobj.WebApiTabs.ToList());
+        }
+
+        // GET: api/API/5
+        [HttpGet]
+        [Route("api/API/getwebapideatiltab/{id}")]
+        public IHttpActionResult Get(int id)
+        {
+            WebApiTab employee = dbobj.WebApiTabs.Find(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return Ok(employee);
+        }
+    
+
+        // POST: api/API
+        [HttpPost]
+        [Route("api/API/postwebapitab")]
+        public IHttpActionResult Post(WebApiTab obj)//(1)
+        {
+            if (ModelState.IsValid)
+            {
+                dbobj.WebApiTabs.Add(obj);// add deatils from (1)
+                dbobj.SaveChanges();
+
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+            return Ok(200);
+        }
+
+        // PUT: api/API/5
+        [HttpPut]
+        [Route("api/API/putwebapitab/{id}")]
+        public IHttpActionResult Put(int id, WebApiTab ob)
+        {
+            dbobj.Entry(ob).State = EntityState.Modified;
+            try
+            {
+                dbobj.SaveChanges();
+            }
+            catch(DbUpdateConcurrencyException)
+            {
+                throw;
+            }
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // DELETE: api/API/5
+        [HttpDelete]
+        [Route("api/API/deletewebapitab/{id}")]
+        
+        public IHttpActionResult Delete(int id)
+        {
+
+            WebApiTab webApiTab = dbobj.WebApiTabs.Find(id);
+            if (webApiTab == null)
+            {
+                return NotFound();
+            }
+            dbobj.WebApiTabs.Remove(webApiTab);
+            dbobj.SaveChanges();
+            return Ok(webApiTab);
+
+
+        }
+    }
+}
---
+++ MVCEntityAPI/Controllers/MVCController.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVCEntityAPI;
+using System.Net.Http;
+
+namespace MVCEntityAPI.Controllers
+{
+    public class MVCController : Controller
+    {
+        private Employee1Entities db = new Employee1Entities();
+
+        // GET: MVC
+        public ActionResult Index()
+        {
+            //return View(db.WebApiTabs.ToList());
+            IEnumerable<WebApiTab> employees = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:59135/api/API/");
+                var responseTask = client.GetAsync("getwebapitabs");
+                responseTask.Wait();
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IList<WebApiTab>>();
+                    readTask.Wait();
+                    employees = readTask.Result;
+                }
+                else
+                {
+                    employees = Enumerable.Empty<WebApiTab>();
+                }
+
+
+
+            }
+            return View(employees);
+        }
+
+        // GET: MVC/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+
+            WebApiTab employee = null;
+            using(var client =new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:59135/api/API/");
+                var responseTask = client.GetAsync($"getwebapideatiltab/{id}");
+                responseTask.Wait();
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<WebApiTab>();
+                    readTask.Wait();
+                    employee = readTask.Result;
+                }
+                else
+                {
+                    employee = new WebApiTab();
+                }
+            }
+            return View(employee);
+
+        }
+
+
+        //    WebApiTab webApiTab = db.WebApiTabs.Find(id);
+        //    if (webApiTab == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(webApiTab);
+        //}
+
+        // GET: MVC/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MVC/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Age,mark")] WebApiTab webApiTab)
+        {
+            if (ModelState.IsValid)
+            {
+                using(var client=new HttpClient())
+                {
+                    client.BaseAddress = new Uri("http://localhost:59135/api/API/");
+                    var postTask = client.PostAsJsonAsync<WebApiTab>("postwebapitab", webApiTab);
+                    postTask.Wait();
+                    var result = postTask.Result;
+                    if (result.IsSuccessStatusCode)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                }
+                return View(webApiTab);
+
+
+
+
+                //db.WebApiTabs.Add(webApiTab);
+                //db.SaveChanges();
+                //return RedirectToAction("Index");
+            }
+
+            return View(webApiTab);
+        }
+
+        // GET: MVC/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+
+            WebApiTab employee = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:59135/api/API/");
+                var responseTask = client.GetAsync($"getwebapideatiltab/{id}");
+                responseTask.Wait();
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<WebApiTab>();
+                    readTask.Wait();
+                    employee = readTask.Result;
+                }
+                else
+                {
+                    employee = new WebApiTab();
+                }
+            }
+            return View(employee);
+        }
+
+        // POST: MVC/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Age,mark")] WebApiTab webApiTab)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri("http://localhost:59135/api/API/");
+                    var postTask = client.PutAsJsonAsync<WebApiTab>($"putwebapitab/ {webApiTab.Id}", webApiTab);
+                    postTask.Wait();
+                    var result = postTask.Result;
+                    if (result.IsSuccessStatusCode)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                }
+            }
+                return View(webApiTab);
+            }
+
+        // GET: MVC/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+
+            WebApiTab employee = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:59135/api/API/");
+                var responseTask = client.GetAsync($"getwebapideatiltab/{id}");
+                responseTask.Wait();
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<WebApiTab>();
+                    readTask.Wait();
+                    employee = readTask.Result;
+                }
+                else
+                {
+                    employee = new WebApiTab();
+                }
+            }
+            return View(employee);
+        }
+
+            // POST: MVC/Delete/5
+            [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:59135/api/API/");
+                var postTask = client.DeleteAsync($"deletewebapitab/{id}");
+                postTask.Wait();
+                var result = postTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+                //WebApiTab webApiTab = db.WebApiTabs.Find(id);
+                //db.WebApiTabs.Remove(webApiTab);
+                //db.SaveChanges();
+                return RedirectToAction("Delete");
+            }
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
